72b29391ec766c57098b4651871dcbb45d1903cb
Roger Spielmann <rspielmann@snowflake.ch>
2014-11-18 16:44:52 +0100

    Product Entity added

--- src/Acme/StoreBundle/Controller/DefaultController.php
+++ src/Acme/StoreBundle/Controller/DefaultController.php
@@ -2,6 +2,8 @@
 
 namespace Acme\StoreBundle\Controller;
 
+use Acme\StoreBundle\Entity\Product;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
@@ -10,4 +12,34 @@ class DefaultController extends Controller
     {
         return $this->render('AcmeStoreBundle:Default:index.html.twig', array('name' => $name));
     }
+
+    public function createAction()
+    {
+        $product = new Product();
+        $product->setName('A Foo Bar');
+        $product->setPrice('19.99');
+        $product->setDescription('Lorem ipsum dolor');
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($product);
+        $em->flush();
+
+        return new Response('Created product id '.$product->getId());
+    }
+
+    public function showAction($id)
+    {
+        $product = $this->getDoctrine()
+            ->getRepository('AcmeStoreBundle:Product')
+            ->find($id);
+
+        if (!$product) {
+            throw $this->createNotFoundException(
+                'No product found for id ' . $id
+            );
+        }
+
+        // ... do something, like pass the $product object into a template
+    }
+
 }
---
+++ src/Acme/StoreBundle/Entity/Product.php
@@ -0,0 +1,104 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rspielmann
+ * Date: 18.11.2014
+ * Time: 14:53
+ */
+
+namespace Acme\StoreBundle\Entity;
+
+
+class Product {
+
+    protected $name;
+
+    protected $price;
+
+    protected $description;
+
+    /**
+     * @var integer
+     */
+    private $id;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Product
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set price
+     *
+     * @param string $price
+     * @return Product
+     */
+    public function setPrice($price)
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    /**
+     * Get price
+     *
+     * @return string 
+     */
+    public function getPrice()
+    {
+        return $this->price;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     * @return Product
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string 
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+}
