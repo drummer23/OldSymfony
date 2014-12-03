f373ee5bb1363a79f06329ba414cbaf39c0f4155
Roger Spielmann <rspielmann@snowflake.ch>
2014-12-03 16:22:06 +0100

    crossfeet action added

--- src/Acme/CalcBundle/Controller/DefaultController.php
+++ src/Acme/CalcBundle/Controller/DefaultController.php
@@ -3,11 +3,24 @@
 namespace Acme\CalcBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
     public function indexAction($name)
     {
         return $this->render('AcmeCalcBundle:Default:index.html.twig', array('name' => $name));
+    }
+
+    public function crossAction($number)
+    {
+        $strDigits = ( string ) $number;
+
+        for( $intCrossfoot = $i = 0; $i < strlen ( $strDigits ); $i++ )
+        {
+            $intCrossfoot += $strDigits{$i};
+        }
+
+        return new Response('<html><body>Number: ' . $intCrossfoot . '</body></html>');
     }
 }
