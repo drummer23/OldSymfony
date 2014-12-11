c0b4b5cf96dcbabed6f767ea7f0e22cda3ded54e
Roger Spielmann <rspielmann@snowflake.ch>
2014-12-11 18:21:53 +0100

    very basic session handling added

--- src/Acme/SessionBundle/Controller/DefaultController.php
+++ src/Acme/SessionBundle/Controller/DefaultController.php
@@ -3,11 +3,26 @@
 namespace Acme\SessionBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\HttpFoundation\Response;
+
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+    public function indexAction()
     {
-        return $this->render('AcmeSessionBundle:Default:index.html.twig', array('name' => $name));
+        $now = new \DateTime();
+
+        $session = new Session();
+
+        //$session->start();
+
+
+        $lastvisit = $session->has('lastvisit') ? $session->get('lastvisit')->format('Y-m-d H:i:s') : 'this is your first visit';
+
+
+        $session->set('lastvisit', $now );
+
+        return new Response('<html><body>Your last visit was: ' . $lastvisit . '</body></html>');
     }
 }
