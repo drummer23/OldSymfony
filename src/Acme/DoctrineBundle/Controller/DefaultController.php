70749538d5f9d684f9c1f989cbfa4e71b8ae936a
Roger Spielmann <rspielmann@snowflake.ch>
2015-01-14 12:13:02 +0100

    createAction added

--- src/Acme/DoctrineBundle/Controller/DefaultController.php
+++ src/Acme/DoctrineBundle/Controller/DefaultController.php
@@ -2,12 +2,28 @@
 
 namespace Acme\DoctrineBundle\Controller;
 
+use Acme\DoctrineBundle\Entity\Person;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\BrowserKit\Response;
 
 class DefaultController extends Controller
 {
     public function indexAction($name)
     {
         return $this->render('AcmeDoctrineBundle:Default:index.html.twig', array('name' => $name));
+    }
+
+    public function createAction($name)
+    {
+        $person = new Person();
+        $person->setName($name);
+        $person->setCity('Zurich');
+
+        $em = $this->getDoctrine()->getManager();
+
+        $em->persist($person);
+        $em->flush();
+
+        return new Response("<html><body>Person $name created</body></html>");
     }
 }
