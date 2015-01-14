8402bb9425b09b9ca68344fc3b38813de430fb6a
Roger Spielmann <rspielmann@snowflake.ch>
2015-01-14 10:58:12 +0100

    basic people entity added

---
+++ src/Acme/DoctrineBundle/Entity/People.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rspielmann
+ * Date: 09.12.2014
+ * Time: 11:24
+ */
+
+namespace Acme\DoctrineBundle\Entity;
+
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="people")
+ */
+class People
+{
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    protected $name;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    protected $city;
+}
