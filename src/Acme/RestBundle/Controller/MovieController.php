d704f0136489cf317414cd06a0fd05374239ebd9
Roger Spielmann <rspielmann@snowflake.ch>
2015-03-05 11:31:43 +0100

    movie controller added

---
+++ src/Acme/RestBundle/Controller/MovieController.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Acme\RestBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class MovieController extends Controller
+{
+}
---
+++ src/Acme/RestBundle/Tests/Controller/MovieControllerTest.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Acme\RestBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class MovieControllerTest extends WebTestCase
+{
+}
