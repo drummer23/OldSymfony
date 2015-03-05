bbe30e7ca9be449587fc1f4f2815406f9095cca0
Roger Spielmann <rspielmann@snowflake.ch>
2015-03-05 10:53:37 +0100

    movie entity added

---
+++ src/Acme/RestBundle/Entity/Movie.php
@@ -0,0 +1,127 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rspielmann
+ * Date: 04.03.2015
+ * Time: 17:33
+ */
+
+namespace Acme\RestBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="movie")
+ */
+class Movie
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string", length=100)
+     */
+    protected $name;
+
+    /**
+     * $var boolean
+     *
+     * @ORM\Column(type="boolean")
+     */
+    protected $seen;
+
+    /**
+     * var integer
+     *
+     * @ORM\Column(type="integer")
+     */
+    protected $rating;
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Movie
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set seen
+     *
+     * @param boolean $seen
+     * @return Movie
+     */
+    public function setSeen($seen)
+    {
+        $this->seen = $seen;
+
+        return $this;
+    }
+
+    /**
+     * Get seen
+     *
+     * @return boolean
+     */
+    public function getSeen()
+    {
+        return $this->seen;
+    }
+
+    /**
+     * Set rating
+     *
+     * @param integer $rating
+     * @return Movie
+     */
+    public function setRating($rating)
+    {
+        $this->rating = $rating;
+
+        return $this;
+    }
+
+    /**
+     * Get rating
+     *
+     * @return integer 
+     */
+    public function getRating()
+    {
+        return $this->rating;
+    }
+}
