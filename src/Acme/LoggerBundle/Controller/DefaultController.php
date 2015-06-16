2c17586ac21dd7129e4a9a6cfd3619a42d2138f8
Roger Spielmann <rspielmann@snowflake.ch>
2015-06-16 16:31:18 +0200

    basic logging implemented

--- src/Acme/LoggerBundle/Controller/DefaultController.php
+++ src/Acme/LoggerBundle/Controller/DefaultController.php
@@ -2,12 +2,23 @@
 
 namespace Acme\LoggerBundle\Controller;
 
+
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+    public function indexAction()
     {
-        return $this->render('AcmeLoggerBundle:Default:index.html.twig', array('name' => $name));
+        /** @var LoggerInterface $logger */
+        $id = uniqid();
+
+        $logger = $this->get('logger');
+        $logger->info(sprintf('{ID: %s} I just got the logger', $id));
+        $logger->info(sprintf('{ID: %s} An error occured', $id));
+
+        return new Response('<html><body>' . sprintf('messages have been logged with prefix {ID: %s}', $id) . '</body></html>');
     }
 }
