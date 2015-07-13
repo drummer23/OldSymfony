9a549d0f6b520d05223a8467204b88d9b46f40e0
Roger Spielmann <rspielmann@snowflake.ch>
2015-07-13 10:42:22 +0200

    adds a basic form

--- src/Acme/UiBundle/Controller/DefaultController.php
+++ src/Acme/UiBundle/Controller/DefaultController.php
@@ -2,7 +2,9 @@
 
 namespace Acme\UiBundle\Controller;
 
+use Acme\UiBundle\Entity\Dog;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
@@ -18,5 +20,21 @@ class DefaultController extends Controller
 
 
         return $this->render('AcmeUiBundle:Default:index.html.twig',array('dogs' => $dogs));
+    }
+
+    public function newAction(Request $request)
+    {
+        $dog = new Dog();
+
+        $form = $this->createFormBuilder($dog)
+            ->add('name','text')
+            ->add('race', 'text')
+            ->add('birthday', 'date')
+            ->add('save', 'submit', array('label' => 'Create Dog'))
+            ->getForm();
+
+        return $this->render('AcmeUiBundle:Default:new.html.twig', array(
+            'form' => $form->createView(),
+        ));
     }
 }
---
+++ src/Acme/UiBundle/Entity/Dog.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace Acme\UiBundle\Entity;
+
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="Acme\UiBundle\Entity\DogRepository")
+ * @ORM\Table(name="dog")
+ */
+class Dog {
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    protected $name;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    protected $race;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    protected $birthday;
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Dog
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set race
+     *
+     * @param string $race
+     * @return Dog
+     */
+    public function setRace($race)
+    {
+        $this->race = $race;
+
+        return $this;
+    }
+
+    /**
+     * Get race
+     *
+     * @return string 
+     */
+    public function getRace()
+    {
+        return $this->race;
+    }
+
+    /**
+     * Set birthday
+     *
+     * @param \DateTime $birthday
+     * @return Dog
+     */
+    public function setBirthday($birthday)
+    {
+        $this->birthday = $birthday;
+
+        return $this;
+    }
+
+    /**
+     * Get birthday
+     *
+     * @return \DateTime 
+     */
+    public function getBirthday()
+    {
+        return $this->birthday;
+    }
+}
