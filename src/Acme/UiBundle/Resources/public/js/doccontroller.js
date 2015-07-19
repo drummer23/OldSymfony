9b2170801a7b0a40313efd3d225d5694ce8fa0b7
Roger Spielmann <puncher@braveheart.ch>
2015-07-19 13:56:12 +0200

    adds javascript to base template

---
+++ src/Acme/UiBundle/Resources/public/js/doccontroller.js
@@ -0,0 +1,17 @@
+app.controller('DogCtrl', function($scope,$http) {
+
+    //init data
+    $scope.dogs = [];
+
+    $http.get('/list').
+        success(function(data, status, headers, config) {
+            $scope.dogs = data.records;
+        }).
+        error(function(data, status, headers, config) {
+            // called asynchronously if an error occurs
+            // or server returns response with an error status.
+        });
+
+
+
+});
